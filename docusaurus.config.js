4a9ecd8a15f371638423d385779b3b85a66a8d71
Amalpv <Amalpv2445@gmail.com>
2021-06-30 12:45:03 +0530

    added code

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,13 +5,14 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 module.exports = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://Amalpv.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Amalpv', // Usually your GitHub org/user name.
+  projectName: 'Amalpv.github.io', //
+  trailingSlash: false,
   themeConfig: {
     navbar: {
       title: 'My Site',
